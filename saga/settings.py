bd73bdb365d7c2b07aa5df0ad8e39172f9c803c6
lohmann94 <lohmann94@hotmail.com>
2023-01-11 15:51:51 +0100

    settings updated

--- saga/settings.py
+++ saga/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4a632=086$w4s_q@bw1uxn$je0@dr%#rurc@tr7(oyzmbp!pz#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
