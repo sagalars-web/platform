0e6af91ce10e9efdbd9ca16eb7ceacaba6bf4c9a
lohmann94 <lohmann94@hotmail.com>
2023-01-12 13:38:19 +0100

    changed default value of datefield

--- core/models.py
+++ core/models.py
@@ -34,13 +34,15 @@ class Post(models.Model):
     def __str__(self):
         return self.user
 
+
 class Member(models.Model):
-    
+
     name = models.CharField(max_length=50, default='john')
     phone = models.IntegerField(default=22222222)
-    creation_date = models.DateField(default=datetime.date.today)
+    creation_date = models.DateField(default=datetime.today)
     email = models.EmailField(default='john@g.com')
-    key_issue = models.CharField(max_length=2000, default='Det her betyder noget for mig')
+    key_issue = models.CharField(
+        max_length=2000, default='Det her betyder noget for mig')
     Birth_year = models.IntegerField(default=1994)
     zip_code = models.IntegerField(default=2100)
     engagement_score = models.IntegerField(default=100)
